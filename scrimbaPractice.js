683f0b25701d20118680bd95ca015515505924c0
Abigail Rick <abigail_rick@mailfence.com>
2024-09-07 08:58:10 -0500

    added css

--- scrimbaPractice.js
+++ scrimbaPractice.js
@@ -1,12 +1,23 @@
-let firstCard = 3
-let secondCard = 9
-
+let firstCard = 12
+let secondCard = 7
 let sum = firstCard + secondCard;
+let hasBlackjack = false
+let isAlive = true;
+let message = "";
 
-if (sum <=20 ){
-   console.log("Do you want to draw a new card?");
+function startGame(){
+
+if (sum <= 20){
+   message = "Do you want to draw a new card?";
 } else if (sum === 21){
-    console.log("Wohoo! You've got Blackjack!");
+    message ="Wohoo! You've got Blackjack!";
+    hasBlackjack = true;
 } else {
-    console.log("You're out of the game!");
+   message = "You're out of the game!";
+    isAlive = false;
 }
+}
+
+//BlackJack
+console.log(isAlive);
+console.log(message);
