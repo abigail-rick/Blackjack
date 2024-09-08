a7745425784813e1493f2eabc88de5c998b08550
Abigail Rick <abigail_rick@mailfence.com>
2024-09-07 19:03:43 -0500

    add functions
    added renderGame() function and newCard() function

--- scrimbaPractice.js
+++ scrimbaPractice.js
@@ -1,23 +1,45 @@
-let firstCard = 12
-let secondCard = 7
+
+let firstCard = 3;
+let secondCard = 1;
+let cards = [firstCard, secondCard];
 let sum = firstCard + secondCard;
-let hasBlackjack = false
+let hasBlackjack = false;
 let isAlive = true;
 let message = "";
+const sumEl = document.getElementById('sum-el');
+const messageEl = document.getElementById('message-el');
+const cardEl = document.getElementById('card-el');
+
+function getRandomCard(){
+   let random = Math.floor(Math.random() * 21);
+   }
+
 
 function startGame(){
+    renderGame();
+}
 
+function renderGame(){
+    cardEl.textContent = "Cards: " + cards[0] + ", " + cards[1];
+    sumEl.textContent = "Sum: " + sum;
 if (sum <= 20){
-   message = "Do you want to draw a new card?";
+   messageEl.textContent = "Do you want to draw a new card?";
 } else if (sum === 21){
-    message ="Wohoo! You've got Blackjack!";
+    messageEl.textContent ="Wohoo! You've got Blackjack!";
     hasBlackjack = true;
 } else {
-   message = "You're out of the game!";
+   messageEl.textContent = "You're out of the game!";
     isAlive = false;
 }
+messageEl.textContent;
 }
 
-//BlackJack
-console.log(isAlive);
-console.log(message);
+function newCard(){
+    let thirdCard = 8;
+    sum += thirdCard;
+    cards.push(thirdCard);
+    renderGame();
+    cardEl.textContent = "Cards: " + cards;
+    
+}
+    
